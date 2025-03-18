dcb8f9d07e82849297fdc0878e27e269d41c4dd6
Sercan Haydaroglu <sercan.haydaroglu@jaduda.com>
2025-03-18 13:12:40 +0100

    revert change

--- src/main/java/org/turquase/pranalyser/application/ServiceFactory.java
+++ src/main/java/org/turquase/pranalyser/application/ServiceFactory.java
@@ -1,6 +1,7 @@
 package org.turquase.pranalyser.application;
 
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.turquase.pranalyser.domain.services.PrStatisticCalculator;
 import org.turquase.pranalyser.infrastructure.github.GithubService;
 import org.turquase.pranalyser.port.in.PrStatisticCalculatorPort;
@@ -13,6 +14,10 @@ public class ServiceFactory {
     }
 
     public static GitRepoServicePort createGithubServicePort(String authToken) {
+        if(StringUtils.isBlank(authToken)) {
+            throw new IllegalArgumentException("Auth token cannot be null or empty.");
+        }
+
         return new GithubService(authToken);
     }
 }
